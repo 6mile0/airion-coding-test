9f506d9bb18505bf3098cffa9d9a5e0befe8e9a1
kakeru hokida <me@6mile.dev>
2024-12-03 08:00:22 +0000

    スキーマ修正

--- backend/schemas/tasks/response.py
+++ backend/schemas/tasks/response.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
-class CreateTaskResponse(BaseModel):
+class TaskResponse(BaseModel):
     task_id: UUID
     title: str
-    description: str
+    description: Optional[str]
     is_done: bool
     created_at: datetime
     updated_at: datetime | None

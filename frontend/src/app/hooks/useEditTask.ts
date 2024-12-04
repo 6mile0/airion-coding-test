e2b4d07a1f074cada0e31e989a7e892e8d5cab09
Kakeru Hokida <me@6mile.dev>
2024-12-04 11:08:50 +0000

    toastを表示するよう修正

--- frontend/src/app/hooks/useEditTask.ts
+++ frontend/src/app/hooks/useEditTask.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { editTask } from "../api/tasks";
 import { TaskRequestBody } from "../schema/tasks/request";
+import { toast, Bounce } from 'react-toastify';
 
 type EditTaskProps = {
     taskRequestBody: TaskRequestBody;
@@ -12,8 +13,32 @@ const useEditTask = ({ taskRequestBody, renewTasks }: EditTaskProps) => {
     
     const handleEditTask = () => {
         editTask(editTaskId, taskRequestBody).then(() => {
+            toast.success('タスクの編集に成功しました', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
             renewTasks();
             setEditTaskId('');
+        }).catch((error) => {
+            console.error(error);
+            toast.error('タスクの編集に失敗しました', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            });
         });
     }
     

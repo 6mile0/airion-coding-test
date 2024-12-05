36b633503eaa88a0835319c142e4dfc09bffa88e
kakeru hokida <me@6mile.dev>
2024-12-05 08:14:40 +0000

    convertDate修正

--- frontend/src/app/utils/convertDate.ts
+++ frontend/src/app/utils/convertDate.ts
@@ -1,4 +1,10 @@
-export const convertDate = (timestamp: string): string => {
+import { zeroPad } from "./zeroPad";
+
+export const convertDate = (timestamp: string, isViewTime: boolean = true): string => {
     const dateObj = new Date(Number(timestamp));
-    return `${dateObj.getFullYear()}/${dateObj.getMonth() + 1}/${dateObj.getDate()}`;
+    if(isViewTime){
+        return `${zeroPad(dateObj.getFullYear(), 4)}/${zeroPad(dateObj.getMonth() + 1, 2)}/${zeroPad(dateObj.getDate(), 2)} ${zeroPad(dateObj.getHours(), 2)}:${zeroPad(dateObj.getMinutes(), 2)}`;
+    }else{
+        return `${zeroPad(dateObj.getFullYear(), 4)}/${zeroPad(dateObj.getMonth() + 1, 2)}/${zeroPad(dateObj.getDate(), 2)}`;
+    }
 }

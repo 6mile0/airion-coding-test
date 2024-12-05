413b3416bc8c674d79b58bcc161576c5f71e1153
kakeru hokida <me@6mile.dev>
2024-12-05 09:06:58 +0000

    検索ロジックの実装

---
+++ frontend/src/app/hooks/search/useSearchTask.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Task } from "../../api/tasks";
+
+export const useSearchTask = (tasks: Task[]) => {
+    const [searchWord, setSearchWord] = useState('');
+    const [result, setResult] = useState<Task[]>([]);
+
+    useEffect(() => {
+        setResult(tasks);
+    }, [tasks]);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const word = e.target.value;
+        setSearchWord(word);
+
+        if (word === '') {
+            setResult(tasks);
+            return;
+        }
+
+        const filteredTasks = tasks.filter((task) => {
+            return task.title.includes(word);
+        });
+        setResult(filteredTasks);
+    }
+
+    return { searchResult: result, searchWord, handleSearch };
+}
